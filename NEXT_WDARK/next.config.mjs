dd10902aa3f1a755f72edc40505e1c3da1548098
Alejandro-front <alejandro@Alejandro>
2025-03-18 21:54:00 -0500

    Next config

--- NEXT_WDARK/next.config.mjs
+++ NEXT_WDARK/next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    localeDetection: true,
-  },
   images: {
     domains: ['dev-wdark-2025.pantheonsite.io'],
   },
